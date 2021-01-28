7eb5ff81ec4daf78baa0102e04fc74f63ddf81d1
zachrodis <zachrodis@gmail.com>
2021-01-28 12:06:23 -0500

    title, favicon, and description metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'KCHEF - Kasese Community Health and Education Foundation',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Working to transform healthcare delivery and educational opportunities in the Rwenzori region of Uganda.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
