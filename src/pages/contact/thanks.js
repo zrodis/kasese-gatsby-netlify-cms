b51b671c78441b572da2f59b9bacb4a456c01397
zachrodis <zachrodis@gmail.com>
2021-01-29 15:45:48 -0500

    contact form thank you

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -6,8 +6,8 @@ export default () => (
     <section className="section">
       <div className="container">
         <div className="content">
-          <h1>Thank you!</h1>
-          <p>This is a custom thank you page for form submissions</p>
+          <h1>Thank you for contacting KCHEF!</h1>
+          <p>We will respond as soon as possible.</p>
         </div>
       </div>
     </section>
